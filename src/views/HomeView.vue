bba4b88c86b696cdb52e47235bb958daeb6c2882
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-03-04 15:36:12 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <hello-world />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import HelloWorld from '../components/HelloWorld.vue'
+
+@Component({ components: { HelloWorld } })
+export default class HomeView extends Vue {
+}
+</script>
